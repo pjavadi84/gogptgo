f08695ecb433880a588ec20d74b0492bae94726a
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2023-03-25 14:09:20 -0700

    added containers and imported them into App.js

--- gogptgo-app/src/App.js
+++ gogptgo-app/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
+import { Brand, CTA, Navbar } from './components';
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3} from './containers';
+
 const App = () => {
     return (
-        <div>
-            <h1>Welcome to GoGPTGo</h1>
+        <div className='App'>
+            <div className='gradient_bg'>
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
     )
 }
