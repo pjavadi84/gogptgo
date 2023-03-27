027ce6d71a52064727727fd2d1a47edf33b7f0bc
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2023-03-27 11:52:13 -0700

    modified navbar components. added images and assets

--- gogptgo-app/src/components/navbar/navbar.jsx
+++ gogptgo-app/src/components/navbar/navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './navbar.css'
+import { RiMenu3Line, RiCloseLine } from 'react-icons';
+
 
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3_navbar-links_logo'></div>
+          <img src={logo} alt='logo' />
+      </div>
       Navbar
     </div>
   )
