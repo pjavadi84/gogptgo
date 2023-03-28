6b5683cfa5a80d06e8028ecfdc60889cbf22d621
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2023-03-28 09:52:28 -0700

    create the state for hiding and showing the navbar's menu items and styling correctly for any device type

--- gogptgo-app/src/components/navbar/navbar.jsx
+++ gogptgo-app/src/components/navbar/navbar.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import './navbar.css'
-import { RiMenu3Line, RiCloseLine } from 'react-icons';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import {useState} from 'react';
 import logo from '../../assets/icons8-corgi-96.png'
+
+const Menu = () => (
+  <>
+    <p><a href='#home'>Home</a></p>
+    <p><a href='#wgpt3'>What is GPT</a></p>
+    <p><a href='#possibility'>Open AI</a></p>
+    <p><a href='#features'>Case Studies</a></p>
+    <p><a href='#blog'>Library</a></p>
+  </>
+)
+  
 
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+
   return (
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
@@ -13,16 +27,29 @@ const Navbar = () => {
       </div>
 
       <div className='gpt3__navbar-links_container'>
-        <p><a href='#home'>Home</a></p>
-        <p><a href='#wgpt3'>What is GPT</a></p>
-        <p><a href='#possibility'>Open AI</a></p>
-        <p><a href='#features'>Case Studies</a></p>
-        <p><a href='#blog'>Library</a></p>
+        <Menu />
       </div>
 
       <div className='gpt3__navbar-sign'>
         <p>Sign In</p>
         <button type="button">Sign Up</button>
+      </div>
+      <div className='gpt3__navbar-menu'>
+        {toggleMenu ? 
+         <RiCloseLine color='#fff' size={27} onClick={()=> setToggleMenu(false)} />
+         : <RiMenu3Line color='#fff' size={27} onClick={()=> setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className='gpt3__navbar-menu_container-links'>
+            <Menu />
+            <div className='gpt3__navbar-menu_container-links-sign'>
+              <p>Sign In</p>
+              <button type="button">Sign Up</button>
+            </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
 
