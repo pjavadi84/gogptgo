ec2a00457342fe4ad7c0c7d4bae5905ab8b87976
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2023-03-28 13:05:00 -0700

    added components the header

--- gogptgo-app/src/containers/header/Header.jsx
+++ gogptgo-app/src/containers/header/Header.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import './header.css';
+import group from '../../assets/Group 81.png'
+import HeaderIlustration from '../../assets/Header Illustration.png'
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='gpt3__header section__padding' id="home">
+      <div className='gpt3__header-content'>
+          <h1>
+              Travel with CorgiAI sounds clever
+          </h1>
+          
+          <p>Future of travelling built by CorgiAI is not unreachable anymore. Joint the revolution</p>
+
+          <div className='gpt3__header-content__input'>
+            <input type="email" placeholder='Your email address' />
+            <button type='button'>Let's Go!</button>
+          </div>
+
+          <div className='gpt3__header-content__group'>
+            <img src={group} />
+            <p>1,600 people requested access a visit in last 24 hours</p>
+          </div>
+
+          <div className='gp3__header-image'>
+            <img src={HeaderIlustration} />
+          </div>
+        </div>
     </div>
   )
 }
